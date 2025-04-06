b4f4a67204eabf0d4c08820e3d4b2c54f194cff8
travertexs <83889363+travertexs@users.noreply.github.com>
2025-04-06 10:22:54 +0000

    Add Gemini 2.0 Flash and Gemini 2.0 Flash-Lite

--- src/generators/google/googleModels.ts
+++ src/generators/google/googleModels.ts
@@ -1,11 +1,15 @@
 export const enum GoogleTextGenModel {
 	GEMINI_1_5_FLASH = "gemini-1.5-flash",
 	GEMINI_1_5_PRO = "gemini-1.5-pro",
+	GEMINI_2_0_FLASH_LITE = "gemini-2.0-flash-lite",
+	GEMINI_2_0_FLASH = "gemini-2.0-flash",
 }
 
 export const googleTextGenModels: Record<GoogleTextGenModel, string> = {
 	[GoogleTextGenModel.GEMINI_1_5_FLASH]: "Gemini 1.5 Flash",
 	[GoogleTextGenModel.GEMINI_1_5_PRO]: "Gemini 1.5 Pro",
+	[GoogleTextGenModel.GEMINI_2_0_FLASH_LITE]: "Gemini 2.0 Flash-Lite",
+	[GoogleTextGenModel.GEMINI_2_0_FLASH]: "Gemini 2.0 Flash",
 };
 
 export const enum GoogleEmbeddingModel {
